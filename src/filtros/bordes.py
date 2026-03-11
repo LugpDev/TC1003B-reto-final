fc0d584cc615c109422767a99ba217ed85addf7d
LugpDev2022 <LugpDev2022@gmail.com>
2026-03-11 12:53:41 -0600

    feat: filtro sobel

--- src/filtros/bordes.py
+++ src/filtros/bordes.py
@@ -1,11 +1,23 @@
 from lib.aplicar_mascara import aplicar_mascara
+from lib.img_suma import img_suma
 
 
-def bordes_horizontales(doc):
-    mask = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
-    return aplicar_mascara(doc, mask, "Bordes horizontales")
+def aplicar_bordes(doc, mask_horizontal, mask_vertical, nombre):
+    [horizontal, _] = aplicar_mascara(doc, mask_horizontal, "Horizontal")
+    [vertical, _] = aplicar_mascara(doc, mask_vertical, "Vertical")
+
+    pixeles = img_suma(doc, horizontal, vertical)
+
+    return [pixeles, nombre]
 
 
-def bordes_verticales(doc):
-    mask = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
-    return aplicar_mascara(doc, mask, "Bordes verticales")
+def prewritt(doc):
+    mask_horizontal = [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]]
+    mask_vertical = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
+    return aplicar_bordes(doc, mask_horizontal, mask_vertical, "Prewitt")
+
+
+def sobel(doc):
+    mask_horizontal = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+    mask_vertical = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+    return aplicar_bordes(doc, mask_horizontal, mask_vertical, "Sobel")
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from filtros.escala_grises import escala_grises
 from filtros.binarizado import binarizado_fijo, binarizado_dinamico
 from filtros.inverso import inverso
 from filtros.suavizado import suavizado_3x3, suavizado_5x5, suavizado_7x7
-from filtros.prewitt import prewritt
+from filtros.bordes import prewritt, sobel
 from lib.crear_boton import crear_boton
 
 
@@ -44,8 +44,11 @@ btn_suavizado5x5.on_clicked(lambda event: editor.aplicar_filtro(suavizado_5x5))
 btn_suavizado7x7 = crear_boton([0.54, 0.96, 0.1, 0.04], "Suavizado 7x7")
 btn_suavizado7x7.on_clicked(lambda event: editor.aplicar_filtro(suavizado_7x7))
 
-btn_bordes_horizontal = crear_boton([0.64, 0.96, 0.1, 0.04], "prewitt")
-btn_bordes_horizontal.on_clicked(lambda event: editor.aplicar_filtro(prewritt))
+btn_prewitt = crear_boton([0.64, 0.96, 0.1, 0.04], "Prewitt")
+btn_prewitt.on_clicked(lambda event: editor.aplicar_filtro(prewritt))
+
+btn_sobel = crear_boton([0.74, 0.96, 0.1, 0.04], "Sobel")
+btn_sobel.on_clicked(lambda event: editor.aplicar_filtro(sobel))
 
 btn_save = crear_boton([0.92, 0.96, 0.08, 0.04], "Guardar")
 btn_save.on_clicked(lambda event: editor.guardar_imagen())
